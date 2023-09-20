f7118601c489ffb83f07386b7a6c88342ea64349
wlarkspur <newacon@naver.com>
2023-09-20 23:59:46 +0200

    Chakra UI setUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react"
+import { Text } from "@chakra-ui/react";
+import React from "react";
 
 function App() {
-  return <div />
-
+  return (
+    <div>
+      <Text color={"red.500"} fontSize={"6xl"}>
+        it works!!
+      </Text>
+    </div>
+  );
 }
 
 export default App;
