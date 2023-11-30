f4e4adc2c6d22a4ec8155d67441f2b96cc372eba
wlarkspur <newacon@naver.com>
2023-12-01 00:57:29 +0200

    /added

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://backend.coolbnb.xyz/api/v1",
+      : "https://backend.coolbnb.xyz/api/v1/",
   withCredentials: true,
 });
 
